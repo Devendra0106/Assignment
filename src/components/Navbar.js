27dfd7114b81297dff822b8ab5c3f8c7cce4298d
Devendra Morankar <devendramorankar@gmail.com>
2021-09-06 21:19:30 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
     return (
         <div className="navbar">
-
+            <div className="left-nav">
+                <div>Menu</div>
+                <div>Create Purchase Receipt</div>
+            </div>
+            <div className="right-nav">
+                <div>New</div>
+                <div>View</div>
+                <div>Print</div>
+                <div>Delete</div>
+                <div>Save as Draft</div>
+                <div>Submit</div>
+            </div>
         </div>
     )
 }
